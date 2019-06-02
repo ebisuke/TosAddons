5274f04ec2c7605a2ba8c985b2cd94435c433f13
fungi <fungi@rect.sakura.ne.jp>
2019-06-02 13:26:25 +0900

    Jumpcancellerの実装

---
+++ findportalshop/src/findportalshop.lua
@@ -0,0 +1,111 @@
+function EBI_try_catch(what)
+    local status, result = pcall(what.try)
+    if not status then
+        what.catch(result)
+    end
+    return result
+end
+function FINDPORTALSHOP_ON_INIT(addon, frame)
+    EBI_try_catch{
+        try = function()
+            frame = ui.GetFrame("findportalshop");
+            frame:ShowWindow(0)
+            if OLD_MAP_OPEN == nil then
+                OLD_MAP_OPEN = MAP_OPEN
+                MAP_OPEN = FINDPORTALSHOP_MAP_OPEN_JUMPER
+            end
+        end,
+        catch = function(error)
+            CHAT_SYSTEM(error)
+        end
+    }
+
+end
+
+function FINDPORTALSHOP_MAP_OPEN_JUMPER(frame)
+    if OLD_MAP_OPEN ~= nil then
+        OLD_MAP_OPEN(frame)
+    end
+    FINDPORTALSHOP_MAP_OPEN(frame)
+end
+
+function FINDPORTALSHOP_MAP_OPEN(frame)
+    EBI_try_catch{
+        try = function()
+            local btnopen = frame:CreateOrGetControl("button", "btnfindportal", 0, 0, 120, 40)
+            btnopen:SetMargin(0, 18, 80 + 120 + 20, 0)
+            btnopen:SetGravity(ui.RIGHT, ui.TOP)
+            btnopen:SetSkinName("test_pvp_btn")
+            btnopen:SetText("{@st66b}ポタ屋一覧")
+            btnopen:SetEventScript(ui.LBUTTONDOWN, "FINDPORTALSHOP_TOGGLE");
+            FINDPORTALSHOP_LISTING();
+        end,
+        catch = function(error)
+            CHAT_SYSTEM(error)
+        end
+    }
+
+end
+function FINDPORTALSHOP_TOGGLE()
+    EBI_try_catch{
+        try = function()
+            ui.GetFrame("map"):ShowWindow(0)
+            local frame = ui.GetFrame("findportalshop");
+            frame:ShowWindow(1)
+            FINDPORTALSHOP_LISTING()
+        end,
+        catch = function(error)
+            CHAT_SYSTEM(error)
+            print(error)
+        end
+    }
+end
+function FINDPORTALSHOP_LISTING()
+    local frame = ui.GetFrame("findportalshop");
+    --比較的遠くまでのポータルショップを探す
+    local objList, objCount = SelectObject(GetMyActor(), 1000, 'ALL')
+    local portals = {}
+    portals = {name = "", title = "", pchandle = 0, mapname = nil, position = nil}
+    portals = {}
+    --ポタ屋を検索
+    for i = 1, objCount do
+        local itm = objList[i];
+        local hnd = GetHandle(itm);
+        local tgt = info.GetTargetInfo(hnd);
+        if (itm.ClassName == "PC") and (tgt.IsDummyPC == 1) then
+            --ダミーPC
+            --グループ名取得
+            --local actr = world.GetActor(hnd);
+            
+            local groupname = info.GetFamilyName(hnd);
+            print(tostring(groupname))
+            if (groupname ~= nil) then
+                --ショップの情報を仕入れる
+                local shop = session.autoSeller.GetShopBaseInfo(AUTO_SELL_PORTAL)
+                if (shop ~= nil) then
+                    print("here")
+                    --ポタ屋
+                    for j = 0, session.autoSeller.GetCount(groupname) - 1 do
+                        
+                        local itemInfo = session.autoSeller.GetByIndex(groupName, i);
+                        local propValue = itemInfo:GetArgStr();
+                        local portalInfoList = StringSplit(propValue, "@"); -- portalPos@openedTime
+                        local portalPosList = StringSplit(portalInfoList[1], "#"); -- zoneName#x#y#z
+                        
+                        -- name
+                        local mapName = portalPosList[1];
+                        local x, y, z = portalPosList[2], portalPosList[3], portalPosList[4];
+                        local mapCls = GetClass('Map', mapName);
+                        local registerCheck = GET_CHILD_RECURSIVELY(ctrlSet, 'registerCheck');
+                        --registerCheck:SetTextByKey('map', mapCls.Name);
+                        portals[#portals] = {name = groupname, title = shop.Title, mapname = mapCls.Name, position = {x = x, y = y, z = z}}
+                    end
+                
+                end
+            end
+        end
+    end
+end
+function FINDPORTALSHOP_FIND(name)
+
+end
---
+++ jumpcanceller/src/jumpcanceller.lua
@@ -0,0 +1,57 @@
+--バフ→skill→の順
+JUMPCANCELLER_SKILLS={}
+JUMPCANCELLER_SKILLS[3018]={type="Skill",clsid=30208}
+JUMPCANCELLER_SKILLS[3076]={type="Skill",clsid=31107}
+
+function EBI_try_catch(what)
+	local status, result = pcall(what.try)
+	if not status then
+		what.catch(result)
+	end
+	return result
+end
+function JUMPCANCELLER_ON_INIT(addon, frame)
+    EBI_try_catch{
+        try = function()
+            frame=ui.GetFrame("jumpcanceller");
+            frame:ShowWindow(1)
+            local timer = GET_CHILD(frame, "addontimer", "ui::CAddOnTimer");
+            
+            timer:SetUpdateScript("JUMPCANCELLER_WATCHKEY");
+            timer:Start(0.01)
+        end,
+        catch = function(error)
+            CHAT_SYSTEM(error)
+        end
+    }
+
+end
+function JUMPCANCELLER_WATCHKEY()
+    EBI_try_catch{
+        try=function()
+
+            local frame=ui.GetFrame("jumpcanceller");
+            --入力方法チェック
+            if(imcinput.HotKey.IsDown("Jump"))then
+                --ON
+
+                --バフが含まれるかチェック
+                local handle = session.GetMyHandle();
+                for i = 0, info.GetBuffCount(handle) - 1 do
+                    local buffout=info.GetBuffIndexed(handle, i).buffID
+                    local skill=JUMPCANCELLER_SKILLS[buffout]
+                    if skill then
+                        --スキルを行使する
+
+                        control.Skill(skill.clsid);
+                    end
+                end
+            
+            end
+        end,
+        catch=function(error)
+            CHAT_SYSTEM(error)
+        end
+    }
+
+end
