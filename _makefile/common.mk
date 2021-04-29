c80490c7815111cd916ab6fab0f03b74d956d95a
ebisuke <fungi@rect.sakura.ne.jp>
2021-04-29 18:35:45 +0900

    create makefile

---
+++ _makefile/common.mk
@@ -0,0 +1,43 @@
+IPF = ipf
+COMPRESSION_LEVEL=9
+MAKEFILE_RELATIVE_DIR = ../_makefile/
+TREEOFSAVIOR_DATA_DIR= ~/E/TosData/
+EMOJI=📖
+SRCDIR=src
+OBJDIR=obj
+BINDIR=bin
+#PATCH=322566
+#VERSION=nothing
+#NAME=undefined
+.DEFAULT_GOAL := generateipf
+
+IPFNAME_WITH_EMOJI=__$(NAME)-$(EMOJI)-v$(VERSION).ipf	
+IPFNAME_WITHOUT_EMOJI=$(NAME)-v$(VERSION).ipf 	
+
+FORCE:
+.PHONY: clean generateipf uninstall deploy install
+
+clean: 
+	@echo clean
+	rm -r $(OBJDIR) | true
+	rm -r $(BINDIR) | true
+$(BINDIR)/$(IPFNAME_WITHOUT_EMOJI):generateipf
+	$(generateipf)
+generateipf: 
+	@echo generateipf
+	$(clean)
+	mkdir $(OBJDIR) | true
+	mkdir $(BINDIR) | true
+	cp -r src/* obj/
+ifdef PATCH
+	$(IPF) -c $(COMPRESSION_LEVEL) -b $(PATCH) -r $(PATCH) $(BINDIR)/$(IPFNAME_WITHOUT_EMOJI) $(OBJDIR)
+else
+	$(IPF) -c $(COMPRESSION_LEVEL) $(BINDIR)/$(IPFNAME_WITHOUT_EMOJI) $(OBJDIR)
+endif
+	cp $(BINDIR)/$(IPFNAME_WITHOUT_EMOJI) $(BINDIR)/$(IPFNAME_WITH_EMOJI)
+install: $(BINDIR)/$(IPFNAME_WITHOUT_EMOJI)
+	@echo install
+	cp -f $(BINDIR)/$(IPFNAME_WITHOUT_EMOJI) $(TREEOFSAVIOR_DATA_DIR)/$(IPFNAME_WITH_EMOJI)
+uninstall: $(TREEOFSAVIOR_DATA_DIR)/$(IPFNAME_WITH_EMOJI)
+	@echo uninstall
+	rm $(TREEOFSAVIOR_DATA_DIR)/$(IPFNAME_WITH_EMOJI)
